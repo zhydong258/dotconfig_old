a32f8033b98d6fba01a409126fe5f70e3fab5491
DONG ZY <ethan.dong@outlook.com>
2018-04-08 13:00:54 +0800

    新增README文件和setup文件，修改i3的config文件。
    README和setup的草稿，还没有具体完成。

---
+++ README.md
@@ -0,0 +1,34 @@
+# README #
+
+This README would normally document whatever steps are necessary to get your application up and running.
+
+### What is this repository for? ###
+
+* Quick summary
+用于集中管理Linux下常用工具的配置文件，方便在一个新的环境中快速建立自己的工作链。
+目前包括：emacs(spacemacs),zsh(oh-my-zsh),i3/i3status
+
+* Version
+[1.0.0]
+
+* [Learn Markdown](https://bitbucket.org/tutorials/markdowndemo)
+
+### How do I get set up? ###
+
+* Summary of set up
+* Configuration
+* Dependencies
+* Database configuration
+* How to run tests
+* Deployment instructions
+
+### Contribution guidelines ###
+
+* Writing tests
+* Code review
+* Other guidelines
+
+### Who do I talk to? ###
+
+* Repo owner or admin
+* Other community or team contact
---
+++ setup.sh
@@ -0,0 +1,78 @@
+#!/usr/bin/env bash
+set -e
+
+ask() {
+  # http://djm.me/ask
+  while true; do
+
+    if [ "${2:-}" = "Y" ]; then
+      prompt="Y/n"
+      default=Y
+    elif [ "${2:-}" = "N" ]; then
+      prompt="y/N"
+      default=N
+    else
+      prompt="y/n"
+      default=
+    fi
+
+    # Ask the question
+    read -p "$1 [$prompt] " REPLY
+
+    # Default?
+    if [ -z "$REPLY" ]; then
+       REPLY=$default
+    fi
+
+    # Check if the reply is valid
+    case "$REPLY" in
+      Y*|y*) return 0 ;;
+      N*|n*) return 1 ;;
+    esac
+
+  done
+}
+
+dir=`pwd`
+if [ ! -e "${dir}/$(basename $0)" ]; then
+  echo "Script not called from within repository directory. Aborting."
+  exit 2
+fi
+dir="${dir}/.."
+
+distro=`lsb_release -si`
+if [ ! -f "dependencies-${distro}" ]; then
+  echo "Could not find file with dependencies for distro ${distro}. Aborting."
+  exit 2
+fi
+
+ask "Install packages?" Y && bash ./dependencies-${distro}
+
+ask "Install python2 modules?" Y && {
+  sudo pip2 install pyyaml
+}
+
+ask "Install symlink for .gitconfig?" Y && ln -sfn ${dir}/.gitconfig ${HOME}/.gitconfig
+ask "Install symlink for .bashrc?" Y && ln -sfn ${dir}/.bashrc ${HOME}/.bashrc
+ask "Install symlink for .bash_profile?" Y && ln -sfn ${dir}/.bash_profile ${HOME}/.bash_profile
+ask "Install symlink for .vimrc?" Y && ln -sfn ${dir}/.vimrc ${HOME}/.vimrc
+ask "Install symlink for .Xresources?" Y && ln -sfn ${dir}/.Xresources ${HOME}/.Xresources
+ask "Install symlink for .xinitrc?" Y && ln -sfn ${dir}/.xinitrc ${HOME}/.xinitrc
+ask "Install symlink for .compton.conf?" Y && ln -sfn ${dir}/.compton.conf ${HOME}/.compton.conf
+ask "Install symlink for .gtkrc-2.0?" Y && ln -sfn ${dir}/.gtkrc-2.0 ${HOME}/.gtkrc-2.0
+ask "Install symlink for .npmrc?" Y && ln -sfn ${dir}/.npmrc ${HOME}/.npmrc
+
+ask "Install symlink for .i3/?" Y && ln -sfn ${dir}/.i3 ${HOME}/.i3
+ask "Install symlink for .vim/?" Y && ln -sfn ${dir}/.vim ${HOME}/.vim
+ask "Install symlink for .bash.d/?" Y && ln -sfn ${dir}/.bash.d ${HOME}/.bash.d
+ask "Install symlink for .config/?" Y && ln -sfn ${dir}/.config ${HOME}/.config
+
+ask "Install symlink for scripts/?" Y && ln -sfn ${dir}/scripts ${HOME}/scripts
+
+# After .vim has been symlinked!
+if [ ! -d "${HOME}/.vim/dein.vim" ]; then
+  echo "Installing dein.vim..."
+  echo "Please run :call dein#install() from vim after this!"
+  curl https://raw.githubusercontent.com/Shougo/dein.vim/master/bin/installer.sh > /tmp/deinvim-installer.sh
+  sh /tmp/deinvim-installer.sh ${HOME}/.vim/dein.vim
+fi
